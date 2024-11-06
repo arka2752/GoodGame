1ed487907148de0e29403e6f4f2685f9892abbfb
arka2752 <arka25253@gmail.com>
2024-11-07 02:34:42 +0600

    second edit

--- app/src/main/java/com/example/goodgame/MainActivity.java
+++ app/src/main/java/com/example/goodgame/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.goodgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView titleView,subtitleView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        titleView = findViewById(R.id.titleview_id);
+        subtitleView =findViewById(R.id.subtitle_id);
+
+        Animation popup = AnimationUtils.loadAnimation(this, R.anim.popup);
+        Animation popup2 = AnimationUtils.loadAnimation(this, R.anim.popup2);
+
+        titleView.startAnimation(popup);
+        subtitleView.startAnimation(popup2);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
     }
 }
